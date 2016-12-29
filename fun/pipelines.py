c86740a70d937fbd9d3945cd16525e7aff8080f6
ZhangBohan <bohanzhang@foxmail.com>
2016-12-29 11:13:33 +0800

    fix download

--- fun/pipelines.py
+++ fun/pipelines.py
@@ -15,9 +15,24 @@ class ImageDownloadPipeline(object):
             images = []
             dir_path = '%s/%s' % (settings.IMAGES_STORE, spider.name)
 
+            request_data = {'allow_redirects': False,
+             'auth': None,
+             'cert': None,
+             'data': {},
+             'files': {},
+             'headers': {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'},
+             'method': 'get',
+             'params': {},
+             'proxies': {},
+             'stream': True,
+             'timeout': 30,
+             'url': '',
+             'verify': True}
+
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
             for image_url in item['image_urls']:
+                request_data['url'] = image_url
                 us = image_url.split('/')[3:]
                 image_file_name = '_'.join(us)
                 file_path = '%s/%s' % (dir_path, image_file_name)
@@ -26,7 +41,7 @@ class ImageDownloadPipeline(object):
                     continue
 
                 with open(file_path, 'wb') as handle:
-                    response = requests.get(image_url, stream=True)
+                    response = requests.request(**request_data)
                     for block in response.iter_content(1024):
                         if not block:
                             break
--- fun/settings.py
+++ fun/settings.py
@@ -19,3 +19,5 @@ IMAGES_STORE = '/tmp/images'
 
 
 DOWNLOAD_DELAY = 0.25    # 250 ms of delay
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
